57dc3cfc4c40b5715edbb87396f4b487e4e6f7fb
WangKai <wangkai@vvpinche.com>
2015-07-02 10:51:17 +0800

    popupWindow

--- app/src/main/java/wangkai/mytest/MainActivity.java
+++ app/src/main/java/wangkai/mytest/MainActivity.java
@@ -1,16 +1,34 @@
 package wangkai.mytest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mPopupBtn;
+    private View contentLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPopupBtn = (Button) findViewById(R.id.btn_popup);
+        mPopupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopup();
+
+            }
+        });
+        contentLayout = findViewById(R.id.ll_main);
     }
 
     @Override
@@ -33,5 +51,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public void showPopup() {
+        View inflate = getLayoutInflater().inflate(R.layout.layout_pinche_select, null);
+        int width = inflate.getWidth();
+        int height = inflate.getHeight();
+        PopupWindow popupWindow = new PopupWindow(inflate, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        popupWindow.setOutsideTouchable(true);
+
+        popupWindow.showAtLocation(contentLayout, Gravity.BOTTOM, 0, 0);
     }
 }
